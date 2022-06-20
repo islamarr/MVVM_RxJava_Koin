84f13d0d348177fe44e0f1069c67756cb291a748
islam.ar <islam.ahmed@approcks.com>
2022-06-20 18:09:19 +0200

    Init jetpack compose

--- app/src/main/java/com/islam/music/MainActivity.kt
+++ app/src/main/java/com/islam/music/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.islam.music
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.islam.music.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.islam.music.ui.theme.MusicTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MusicTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MusicTheme {
+        Greeting("xccc")
     }
 }
