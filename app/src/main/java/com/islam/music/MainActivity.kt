1412ad360b8e956ba3d037107a3cfd41e95bb02c
islam.ar <islam.ahmed@approcks.com>
2022-06-21 13:55:10 +0200

    State hoisting

--- app/src/main/java/com/islam/music/MainActivity.kt
+++ app/src/main/java/com/islam/music/MainActivity.kt
@@ -3,10 +3,13 @@ package com.islam.music
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.islam.music.ui.theme.MusicTheme
@@ -23,33 +26,77 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+private fun MyApp() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Column (modifier = Modifier.padding(vertical = 4.dp)){
-            for (name in names) {
-                Greeting(name)
-            }
-        }
+        var shouldShowOnboarding by remember { mutableStateOf(true) }
 
+        if (shouldShowOnboarding) {
+            OnboardingScreen {
+                shouldShowOnboarding = false
+            }
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+    val updatedWord = remember { mutableStateOf(name) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Hello, ")
-                Text(text = name)
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .clickable(onClick = { updatedWord.value = "Done" })
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ", color = Color.White)
+                Text(text = updatedWord.value, color = Color.White)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text("Show less")
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded) "Show less" else "Show more", color = Color.White)
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
             }
         }
     }
@@ -59,6 +106,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MusicTheme {
-        MyApp()
+        OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
     }
 }
